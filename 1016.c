031c4a71145350347eeb842afbcaac19ed8697de
José Carlos <josecarlosvieirarn@gmail.com>
2018-10-16 23:08:11 -0300

    Exercício 1016

---
+++ 1016.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int dist, temp;
+
+	printf("Exercicio 1016 \n");
+	printf("---------------\n");
+	printf("Digite a distancia dos veiculos: \n");
+	scanf("%d", &dist);
+
+	temp = dist * 2;	 
+	
+	printf("---------------\n");
+	printf("%d minutos\n", temp);
+	
+
+	return 0;
+}
